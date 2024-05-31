871378f9eabb02b3abd4779c2f8e90a4dfef0654
Ashu Desai <ashuddesai@gmail.com>
2024-05-30 19:22:51 -0700

    updating site to use SSR for og params

---
+++ web/render.js
@@ -0,0 +1,58 @@
+const https = require('https');
+const querystring = require('querystring');
+
+function fetchPageData(slug) {
+  const queryString = querystring.stringify({ slug });
+  return new Promise((resolve, reject) => {
+    https.get(`https://prod.seekmaya.com/og?${queryString}`, res => {
+      let data = '';
+      res.on('data', chunk => (data += chunk));
+      res.on('end', () => resolve(JSON.parse(data)));
+      res.on('error', reject);
+    });
+  });
+}
+
+exports.handler = async (event, context) => {
+  let pageData = {
+    title: 'Maya',
+    description:
+      'Maya is an app to help you learn and discover new perspectives.',
+    image: 'https://seekmaya.com/icon-512-maskable.png',
+  };
+  try {
+    const fetchedData = await fetchPageData('/your-slug');
+    pageData = { ...pageData, ...fetchedData };
+  } catch (err) {
+    console.error(err);
+  }
+
+  const fullHtml = `
+    <!doctype html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.ico" />
+        <title>${pageData.title}</title>
+        <meta name="description" content="${pageData.description}" />
+        <meta property="og:title" content="${pageData.title}" />
+        <meta property="og:description" content="${pageData.description}" />
+        <meta
+          property="og:image"
+          content="${pageData.image}"
+        />
+      </head>
+      <body>
+        <div id="maya-root"></div>
+        <script src="/bundle.web.js"></script>
+      </body>
+    </html>
+  `;
+
+  return {
+    statusCode: 200,
+    headers: { 'Content-Type': 'text/html' },
+    body: fullHtml,
+  };
+};
--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,29 +1,19 @@
-// web/webpack.config.js
-
 const path = require('path');
 const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
 const appDirectory = path.resolve(__dirname, '../');
 
-const compileNodeModules = [
-  // Add every react-native package that needs compiling
-  // 'react-native-gesture-handler',
-  'react-native-reanimated',
-].map(moduleName => path.resolve(appDirectory, `node_modules/${moduleName}`));
+const compileNodeModules = ['react-native-reanimated'].map(moduleName =>
+  path.resolve(appDirectory, `node_modules/${moduleName}`),
+);
 
-// This is needed for webpack to compile JavaScript.
-// Many OSS React Native packages are not compiled to ES5 before being
-// published. If you depend on uncompiled packages they may cause webpack build
-// errors. To fix this webpack can be configured to compile to the necessary
-// `node_module`.
 const babelLoaderConfiguration = {
   test: /\.(js|jsx|ts|tsx)$/,
-  // Add every directory that needs to be compiled by Babel during the build.
   include: [
     path.resolve(appDirectory, 'web/index.web.js'),
     path.resolve(appDirectory, 'web/maya.web.tsx'),
+    // path.resolve(appDirectory, 'web/render.js'),
     path.resolve(appDirectory, 'src'),
     ...compileNodeModules,
   ],
@@ -31,14 +21,12 @@ const babelLoaderConfiguration = {
     loader: 'babel-loader',
     options: {
       cacheDirectory: false,
-      // The 'metro-react-native-babel-preset' preset is recommended to match React Native's packager
       presets: [
         'module:metro-react-native-babel-preset',
-        '@babel/preset-typescript', // Add this preset to handle TypeScript
+        '@babel/preset-typescript',
         '@babel/preset-env',
         '@babel/preset-react',
       ],
-      // Re-write paths to import only the modules needed by the app
       plugins: [
         'react-native-web',
         '@babel/plugin-transform-react-jsx',
@@ -50,7 +38,6 @@ const babelLoaderConfiguration = {
   },
 };
 
-// This is needed for webpack to import static images in JavaScript files.
 const imageLoaderConfiguration = {
   test: /\.(gif|jpe?g|png|svg)$/,
   use: {
@@ -64,29 +51,10 @@ const imageLoaderConfiguration = {
 
 const cssLoaderConfiguration = {
   test: /\.css$/,
-  use: [
-    'style-loader', // Injects styles into DOM
-    'css-loader', // Translates CSS into CommonJS
-  ],
+  use: ['style-loader', 'css-loader'],
 };
 
-module.exports = {
-  entry: [
-    // load any web API polyfills
-    // path.resolve(appDirectory, 'polyfills-web.js'),
-    // your web-specific entry file
-    path.resolve(appDirectory, 'web/index.web.js'),
-  ],
-
-  // configures where the build ends up
-  output: {
-    filename: 'bundle.web.js',
-    path: path.resolve(appDirectory, 'dist'),
-    publicPath: '/',
-  },
-
-  // ...the rest of your config
-
+const commonConfig = {
   module: {
     rules: [
       babelLoaderConfiguration,
@@ -94,7 +62,6 @@ module.exports = {
       cssLoaderConfiguration,
     ],
   },
-
   resolve: {
     alias: {
       'react-native$': 'react-native-web',
@@ -110,23 +77,22 @@ module.exports = {
       '.jsx',
     ],
   },
-  devServer: {
-    static: {
-      directory: path.join(appDirectory, 'assets'), // Serve content from the assets directory
-      publicPath: '/',
-      watch: true,
-    },
-    historyApiFallback: true, // This is crucial for single-page applications
-    hot: true, // Enable hot module replacement
-    open: true, // Open the browser after the server has been started
-    port: 3000, // Port to run the server on
+};
+
+const clientConfig = {
+  ...commonConfig,
+  entry: [path.resolve(appDirectory, 'web/index.web.js')],
+  output: {
+    filename: 'bundle.web.js',
+    path: path.resolve(appDirectory, 'web/netlify/dist'),
+    publicPath: '/',
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(appDirectory, 'web/index.html'),
-      favicon: path.join(appDirectory, 'assets/app icons/web/favicon.ico'),
-    }),
-    new webpack.HotModuleReplacementPlugin(), // Enable HMR
+    // new HtmlWebpackPlugin({
+    //   template: path.join(appDirectory, 'web/index.html'),
+    //   favicon: path.join(appDirectory, 'assets/app icons/web/favicon.ico'),
+    // }),
+    new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
       __DEV__: JSON.stringify(true),
     }),
@@ -134,12 +100,37 @@ module.exports = {
     new webpack.DefinePlugin({ process: { env: {} } }),
     new CopyPlugin({
       patterns: [
-        { from: 'web/_redirects', to: '' },
+        // { from: 'web/_redirects', to: '' },
+        { from: 'web/render.js', to: '../functions' },
         { from: 'assets/app icons/web/icon-512-maskable.png', to: '' },
       ],
     }),
-    // new CopyWebpackPlugin({
-    //   patterns: [{ from: path.resolve(appDirectory, 'assets'), to: 'assets' }],
-    // }),
   ],
+  devServer: {
+    static: {
+      directory: path.join(appDirectory, 'assets'),
+      publicPath: '/',
+      watch: true,
+    },
+    historyApiFallback: true,
+    hot: true,
+    open: true,
+    port: 3000,
+  },
 };
+
+// const serverConfig = {
+//   ...commonConfig,
+//   target: 'node',
+//   externals: [nodeExternals()],
+//   entry: {
+//     render: path.resolve(appDirectory, 'web/render.js'), // Only this entry point is needed
+//   },
+//   output: {
+//     filename: '[name].js',
+//     path: path.resolve(appDirectory, 'web/netlify/functions'),
+//     publicPath: '/',
+//   },
+// };
+
+module.exports = [clientConfig];
