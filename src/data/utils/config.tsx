4985659baf28a412c34ce3ef0afa1e000f018c80
Ashu Desai <ashuddesai@gmail.com>
2025-03-19 13:46:41 -0700

    adding local config

--- src/data/utils/config.tsx
+++ src/data/utils/config.tsx
@@ -7,7 +7,7 @@ import { logger } from '@/data/utils/funx';
 // Overrides for development purposes
 
 ///// Environment /////
-export const ENV: Environment = 'prod';
+export const ENV: Environment = 'local';
 
 ///// Overrides nav stack  /////
 export let DEV_SCREEN: Screen;
